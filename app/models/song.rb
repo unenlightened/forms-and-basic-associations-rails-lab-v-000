ec4cc94f05c5983b67b73dcb8a04ff78528b8bf3
Chely Ho <adept_inept@yahoo.com>
2018-04-11 04:23:43 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,11 +3,11 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: name)
-  end
-
-  def genre_name
-    self.genre.name
-  end
+  # def genre_name=(name)
+  #   self.genre = Genre.find_or_create_by(name: name)
+  # end
+  # 
+  # def genre_name
+  #   self.genre.name
+  # end
 end
