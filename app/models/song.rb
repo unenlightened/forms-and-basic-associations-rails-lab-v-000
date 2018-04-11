273fd886d68f88501c305c558002849deda3c1fe
Chely Ho <adept_inept@yahoo.com>
2018-04-11 04:23:52 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,7 @@ class Song < ActiveRecord::Base
   # def genre_name=(name)
   #   self.genre = Genre.find_or_create_by(name: name)
   # end
-  # 
+  #
   # def genre_name
   #   self.genre.name
   # end
